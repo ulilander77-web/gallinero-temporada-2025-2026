551e242e27c1a607056fae74cdcd5664fe5f81d6
ulilander77-web <ulilander77@gmail.com>
2025-10-15 14:43:05 -0300

    Agrego page.tsx

---
+++ prisma/prisma/app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Gallinero Temporada 2025-2026</h1>
+      <p>¡Bienvenido a la app de gestión de gallinas!</p>
+    </main>
+  )
+}
